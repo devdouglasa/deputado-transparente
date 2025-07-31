ffcd8676a6f09fc112e518db979aa9254280bff7
devdouglasa <douglasalisson27@gmail.com>
2025-07-31 20:43:50 -0300

    feat: added deputy index api route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DeputyController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::group(['prefix'=> 'v1'], function () {
+    Route::get('/deputies', [DeputyController::class, 'index'])->name('deputies');
+});
