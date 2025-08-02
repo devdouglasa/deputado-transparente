434945167c1a1ec03c03fdeb403ba05825cebab6
devdouglasa <douglasalisson27@gmail.com>
2025-08-01 23:36:34 -0300

    feat: added job to run every day

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Jobs\SyncDeputiesExpensesJob;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::job(new SyncDeputiesExpensesJob(), 'SyncDeputiesExpensesJob', 'database')->daily();
