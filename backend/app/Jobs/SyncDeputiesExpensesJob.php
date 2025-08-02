f07123b684bed4665c8715ae99fea3460054c992
devdouglasa <douglasalisson27@gmail.com>
2025-08-01 23:34:49 -0300

    feat: created deputy synchronization queue job

---
+++ backend/app/Jobs/SyncDeputiesExpensesJob.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Http;
+use App\Models\Deputie;
+
+class SyncDeputiesExpensesJob implements ShouldQueue
+{
+    use Queueable, Dispatchable, InteractsWithQueue, SerializesModels;
+
+    public $timeout = 2400;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $deputies = Http::get("https://dadosabertos.camara.leg.br/api/v2/deputados")['dados'];
+
+        foreach ($deputies as $dep)
+        {
+            $depInfoDetail = Http::get('https://dadosabertos.camara.leg.br/api/v2/deputados/' . $dep['id'])['dados'];
+
+            Deputie::updateOrCreate(
+                ['id' => $dep['id']],
+                [
+                    'nome' => $dep['nome'],
+                    'sigla_uf' => $dep['siglaUf'],
+                    'sigla_partido' => $dep['siglaPartido'],
+                    'url_foto' => $dep['urlFoto'],
+                    'nome_civil' => $depInfoDetail['nomeCivil'],
+                    'sexo' => $depInfoDetail['sexo'],
+                    'data_nascimento' => $depInfoDetail['dataNascimento'],
+                ]
+            );
+        }
+    }
+}
