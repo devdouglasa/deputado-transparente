249965c01bbbd349181a9c7a1fcba99002258b01
devdouglasa <douglasalisson27@gmail.com>
2025-08-03 18:29:24 -0300

    feat: MP expense synchronization job created

---
+++ backend/app/Jobs/SyncExpensesJob.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use illuminate\Support\Facades\Http;
+use App\Models\Deputie;
+use App\Models\Expense;
+use Exception;
+
+class SyncExpensesJob implements ShouldQueue
+{
+    use Queueable;
+
+    public $timeout = 2400;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $deputies = Deputie::get();
+
+        if (empty($deputie))
+        {
+            return throw new Exception("Não foram encontradas informações de deputados!");
+        }
+
+        foreach ($deputies as $deputie) {
+            $expenses = Http::get("https://dadosabertos.camara.leg.br/api/v2/deputados/" . $deputie['id'] . "/despesas")['dados'];
+
+            foreach ($expenses as $expense) {
+                Expense::updateOrCreate(
+                    ['deputado_id' => $deputie['id'], 'data_documento' => $expense['dataDocumento'], 'tipo_despesa' => $expense['tipoDespesa']],
+                    [
+                        'deputado_id' => $deputie['id'],
+                        'ano' => $expense['ano'],
+                        'mes' => $expense['mes'],
+                        'tipo_despesa' => $expense['tipoDespesa'],
+                        'fornecedor' => $expense['nomeFornecedor'],
+                        'valor_documento' => $expense['valorDocumento'],
+                        'data_documento' => $expense['dataDocumento'],
+                        'num_documento' => $expense['numDocumento'],
+                    ]
+                );
+            }
+
+        }
+    }
+}
