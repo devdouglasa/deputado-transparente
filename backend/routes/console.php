e5e33ad48a21fa4416a6ebf307c157967e427f64
devdouglasa <douglasalisson27@gmail.com>
2025-08-03 18:38:17 -0300

    refactor: refactored deputy task queue and expenses to synchronize every day

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use App\Jobs\SyncDeputiesExpensesJob;
+use App\Jobs\SyncDeputiesJob;
+use App\Jobs\SyncExpensesJob;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
 
-Schedule::job(new SyncDeputiesExpensesJob(), 'SyncDeputiesExpensesJob', 'database')->daily();
+Schedule::job(new SyncDeputiesJob(), connection: 'database')->daily();
+Schedule::job(new SyncExpensesJob(), connection: 'database')->daily();
