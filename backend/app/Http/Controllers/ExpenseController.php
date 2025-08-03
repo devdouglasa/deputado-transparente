f71798b9ffa8a0a1cb2f7a433c88d1887ae464f0
devdouglasa <douglasalisson27@gmail.com>
2025-08-03 11:56:14 -0300

    feat: added expense return responsibility to controller Expense

--- backend/app/Http/Controllers/ExpenseController.php
+++ backend/app/Http/Controllers/ExpenseController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Models\Expense;
+use App\Models\Deputie;
 
 class ExpenseController extends Controller
 {
-    //
+    function show(int $id)
+    {
+        $deputy = Deputie::findOrFail($id);
+
+        $expenses = Expense::query()->where("deputado_id", $deputy->id)->get();
+
+        return response()->json($expenses);
+    }
 }
